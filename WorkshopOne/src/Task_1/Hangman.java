a2d08505819c4dd55bd5a9665142af995e8e22ae
unknown <IvyLeano@outlook.com>
2019-05-18 21:07:26 -0400

    Second Draft Updates

--- WorkshopOne/src/Task_1/Hangman.java
+++ WorkshopOne/src/Task_1/Hangman.java
@@ -5,59 +5,95 @@ import java.lang.Math;
 
 
 public class Hangman {
-	public static void main(String[] args) {
-		String[] colour = {"blue", "orange", "yellow", "brown", "green", "purple"};
-		int colourIndex = (int)(1 + Math.random() * 5);
-		int colourLength = colour[colourIndex].length();
-		char[] userParsedColour = new char[colourLength];
-		char[] parsedColour = new char[colourLength];
-		int lettersRemaining = colourLength;
+	//I created this class to make re-setting the game easier
+	public static class Data {
+	    String colour;
+		int colourLength;
+		char[] userParsedColour;
+		char[] parsedColour;
+		int lettersRemaining;
+		String userStartingString;
+		int totalMisses;
 		
+		void setData() {
+			String[] colourOptions = {"blue", "orange", "yellow", "brown", "green", "purple"};
+			int index = (int)(1 + Math.random() * 5);
+			totalMisses = 0;
+			colour = colourOptions[index];
+			colourLength = colour.length();
+			
+			lettersRemaining = colourLength;
+			userParsedColour = new char[colourLength];
+			parsedColour = new char[colourLength];
+			
+			for(int i = 0; i < colourLength; i++) {
+				userParsedColour[i] = '*';
+				parsedColour[i] = colour.charAt(i);
+				
+			}
+			userStartingString = new String(userParsedColour);
+			
+			
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		//Instantiate a new Data object called newGame
+		//and invoked the setData() function to set the data values for the game
+		Data newGame = new Data();
+		newGame.setData();
+		
+		//Hangman Greeting
 		System.out.println("__________________________________________\n");
 		System.out.println("          Welcome to Hangman!\n");
 		System.out.println("__________________________________________\n");
 		System.out.println("The category is: Colours");
-		System.out.println("The colour I'm thinking of has " + colourLength + " letters.");
-		System.out.printf("Take a guess, enter a letter in: ");
+		System.out.println("The colour I'm thinking of has " + newGame.colourLength + " letters.");
+		System.out.printf("Take a guess, enter a letter in: " + newGame.userStartingString);
 		
-		for(int i = 0; i < colourLength; i++) {
-			System.out.printf("*");
-			userParsedColour[i] = '*';
-			parsedColour[i] = colour[colourIndex].charAt(i);
-			
-		}
-		System.out.printf("\n");
 		
-		//System.out.print(parsedCountry.toString());
+		while(newGame.lettersRemaining != 0) {
 		
-		while(lettersRemaining != 0) {
-		
-		System.out.printf("\n");
+		System.out.println("\n");
 		System.out.printf("Your Guess: ");
 		Scanner input = new Scanner(System.in);
 		char letter = input.next().charAt(0); //nextChar() does not exist, so returns the first character in a string input instead
 		
 		System.out.printf("Result: ");
-		System.out.printf("\n");
-		for(int i = 0; i < colourLength; i++) {
-			if(Character.toLowerCase(letter) == parsedColour[i]) {
-				userParsedColour[i] = letter;
-				lettersRemaining--;
-				//System.out.println(lettersRemaining);
+	
+		for(int i = 0; i < newGame.colourLength; i++) {
+			if(Character.toLowerCase(letter) == newGame.parsedColour[i]) {
+				newGame.userParsedColour[i] = letter;
+				newGame.lettersRemaining--;
 			}
-				System.out.print(userParsedColour[i]);
+			else {
+				newGame.totalMisses++;
+			}
+				System.out.print(newGame.userParsedColour[i]);
 			
 		}
-		if(lettersRemaining == 0) {
-			System.out.println("\nCongratulations! You guessed correctly.\n Would you like to play again? Enter y or n: ");
+		System.out.printf("\n");
+		if(newGame.lettersRemaining == 0) {
+			System.out.println("__________________________________________\n");
+			System.out.println("Congratulations! You guessed correctly, the colour is " + newGame.colour + "!");
+			System.out.println("Game Statistics: ");
+			System.out.println("Total Misses: " + newGame.totalMisses);
+			
+			System.out.printf("Would you like to play again? Enter y or n: ");
 			char playAgain = input.next().charAt(0);
+			
+			
 			if(Character.toLowerCase(playAgain) == 'y') {
-				lettersRemaining = colourLength;
-				
+				newGame.setData();
+				System.out.println("The colour I'm thinking of has " + newGame.colourLength + " letters.");
 			}
+			
+			System.out.println("__________________________________________\n");
 		}
 		}
-		
 		
 	}
+	
 }
