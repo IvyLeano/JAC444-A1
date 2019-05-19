e2256f2d53a66ebb5160bbe190a2d85ff6135409
unknown <IvyLeano@outlook.com>
2019-05-19 18:50:28 -0400

    Working app, user input validation for spaces needs to be fixed

--- WorkshopOne/src/Task_1/Hangman.java
+++ WorkshopOne/src/Task_1/Hangman.java
@@ -1,99 +1,149 @@
-//To resolve the "declared package does not match the expected package" error
-//package Task_1;
 import java.util.Scanner;
 import java.lang.Math;
 
-
 public class Hangman {
-	//I created this class to make re-setting the game easier
-	public static class Data {
-	    String colour;
-		int colourLength;
-		char[] userParsedColour;
-		char[] parsedColour;
-		int lettersRemaining;
-		String userStartingString;
-		int totalMisses;
+	public static class GameData {
+		int m_colourLength;
+		int m_lettersRemaining;
+		char[] m_userKey;
+		char[] m_guessedLetters;
+		char[] m_answerKey;
+		int m_guessedLetterIndex;
+		int m_incorrectGuesses;
 		
-		void setData() {
-			String[] colourOptions = {"blue", "orange", "yellow", "brown", "green", "purple"};
-			int index = (int)(1 + Math.random() * 5);
-			totalMisses = 0;
-			colour = colourOptions[index];
-			colourLength = colour.length();
+		void setGame() {
+			String[] colours = { "blue", "orange", "yellow", "brown", "green", "purple" };
+			int index = (int) (1 + Math.random() * 5);
+			m_colourLength = colours[index].length();
+			m_lettersRemaining = m_colourLength;
+			m_userKey = new char[m_colourLength];
+			m_guessedLetters = new char[20];
+			m_answerKey = new char[m_colourLength];
+			m_guessedLetterIndex = 0;
+			m_incorrectGuesses = 0;
 			
-			lettersRemaining = colourLength;
-			userParsedColour = new char[colourLength];
-			parsedColour = new char[colourLength];
-			
-			for(int i = 0; i < colourLength; i++) {
-				userParsedColour[i] = '*';
-				parsedColour[i] = colour.charAt(i);
-				
+			for (int i = 0; i < m_colourLength; i++) {
+				m_userKey[i] = '*';
+				m_answerKey[i] = colours[index].charAt(i);
+
 			}
-			userStartingString = new String(userParsedColour);
-			
-			
+		}
+		
+		boolean validateLetter(char letter) {
+			boolean isLetterAlreadyPlayed = false;
+			String colour = new String(m_answerKey);
+	
+			if (m_guessedLetters[0] != '\0') { 
+				for (int i = 0; i < 10; i++) {
+					if (m_guessedLetters[i] == letter) {
+						System.out.printf("You have already entered that value.\n");
+						System.out.printf("Please try again. Enter a letter.");
+						isLetterAlreadyPlayed = true;
+						break;
+					}
+				}
+			if(m_guessedLetters[19] != '\0') {
+				System.out.println("You have used 20 guesses, the correct answer is: " + colour);
+				closingStatements();
+			}
+			}
+			return isLetterAlreadyPlayed;
+		}
+		void closingStatements() {
+			Scanner input = new Scanner(System.in);
+			String startingString;
+			char playAgain;
+			System.out.println("Game Statistics: ");
+			System.out.println("Total Misses: " + m_incorrectGuesses);
+
+			System.out.printf("\nWould you like to play again? Enter y or n: ");
+			playAgain = input.next().charAt(0);
+			System.out.println();
+			if (Character.toLowerCase(playAgain) == 'y') {
+				setGame();
+				startingString = new String(m_userKey);
+				System.out.println("The colour I'm thinking of has " + m_colourLength + " letters.");
+				System.out.println("Take a guess, enter a letter in: " + startingString);
+			}
 		}
 	}
-	
 	
 	public static void main(String[] args) {
-		
-		//Instantiate a new Data object called newGame
-		//and invoked the setData() function to set the data values for the game
-		Data newGame = new Data();
-		newGame.setData();
-		
-		//Hangman Greeting
-		System.out.println("__________________________________________\n");
-		System.out.println("          Welcome to Hangman!\n");
-		System.out.println("__________________________________________\n");
-		System.out.println("The category is: Colours");
-		System.out.println("The colour I'm thinking of has " + newGame.colourLength + " letters.");
-		System.out.printf("Take a guess, enter a letter in: " + newGame.userStartingString);
-		
-		
-		while(newGame.lettersRemaining != 0) {
-		
-		System.out.println("\n");
-		System.out.printf("Your Guess: ");
+		GameData newGame = new GameData();
+		newGame.setGame();
 		Scanner input = new Scanner(System.in);
-		char letter = input.next().charAt(0); //nextChar() does not exist, so returns the first character in a string input instead
-		
-		System.out.printf("Result: ");
+		String startingString = new String(newGame.m_userKey);
+		String colour = new String(newGame.m_answerKey);
+		String userInput;
+		char letter;
+		boolean match;
 	
-		for(int i = 0; i < newGame.colourLength; i++) {
-			if(Character.toLowerCase(letter) == newGame.parsedColour[i]) {
-				newGame.userParsedColour[i] = letter;
-				newGame.lettersRemaining--;
-			}
-			else {
-				newGame.totalMisses++;
-			}
-				System.out.print(newGame.userParsedColour[i]);
-			
-		}
-		System.out.printf("\n");
-		if(newGame.lettersRemaining == 0) {
-			System.out.println("__________________________________________\n");
-			System.out.println("Congratulations! You guessed correctly, the colour is " + newGame.colour + "!");
-			System.out.println("Game Statistics: ");
-			System.out.println("Total Misses: " + newGame.totalMisses);
-			
-			System.out.printf("Would you like to play again? Enter y or n: ");
-			char playAgain = input.next().charAt(0);
-			
-			
-			if(Character.toLowerCase(playAgain) == 'y') {
-				newGame.setData();
-				System.out.println("The colour I'm thinking of has " + newGame.colourLength + " letters.");
-			}
-			
-			System.out.println("__________________________________________\n");
-		}
-		}
 		
+		// Hangman Greeting
+				System.out.println("_____________________________________________________________________\n");
+				System.out.println("                        Welcome to Hangman!\n");
+				System.out.println("_____________________________________________________________________\n");
+				System.out.println("The category is: Colours");
+				System.out.println("The colour I'm thinking of has " + newGame.m_colourLength + " letters.");
+				System.out.println("You have 20 tries before the answer will be revealed. Good Luck!\n");
+				System.out.printf("Take a guess, enter a letter: " + startingString);
+				
+				guess: while(newGame.m_lettersRemaining != 0) {
+					
+					System.out.println("\n");
+					System.out.printf("Your Guess: ");
+					userInput = input.next();
+					
+					letter = userInput.charAt(0);
+					
+					//fix this here to remove white spaces from a program in java
+			        //
+			        String  userInput2 = userInput.replaceAll("6", "");
+					System.out.println( userInput2 + "_____________________________________________________________________\n");
+
+					
+					boolean invalidInput = userInput2.length() > 1 || ((int)letter >= 91 && (int)letter <= 96) || ((int)letter >= 32 && (int)letter <= 64) ||
+					((int)letter >= 123 && (int)letter <= 127);
+					
+					if(invalidInput) {
+						System.out.printf("Your input was invalid. Please enter a single alphabetical character");
+						continue guess;
+					}
+					if(newGame.validateLetter(letter) == true) {
+						continue guess;
+					}
+					
+					newGame.m_guessedLetters[newGame.m_guessedLetterIndex] = letter;
+					newGame.m_guessedLetterIndex++;
+					
+					System.out.printf("Result: ");
+					match = false;
+					
+					for (int i = 0; i < newGame.m_colourLength; i++) {
+						if (Character.toLowerCase(letter) == newGame.m_answerKey[i]) {
+							newGame.m_userKey[i] = letter;
+							newGame.m_lettersRemaining--;
+							match = true;
+
+						}
+						if (i == newGame.m_colourLength - 1 && match == false) {
+							newGame.m_incorrectGuesses++;
+
+						}
+						System.out.print(newGame.m_userKey[i]);
+
+		
+						if (newGame.m_lettersRemaining == 0) {
+							System.out.print("\n_____________________________________________________________________\n");
+							System.out.println("Congratulations! You guessed correctly, the colour is " + colour + "!");
+							newGame.closingStatements();
+							
+						}
+					
+					
+				}
+		//input.close();
 	}
 	
+	}
 }
