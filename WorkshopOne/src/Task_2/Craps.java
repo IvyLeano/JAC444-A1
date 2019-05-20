9bc785f422185dc05c021ace64a7e9f6ad054d6b
unknown <IvyLeano@outlook.com>
2019-05-20 17:58:09 -0400

    Pause() added

--- WorkshopOne/src/Task_2/Craps.java
+++ WorkshopOne/src/Task_2/Craps.java
@@ -12,36 +12,20 @@ public class Craps {
 			die2 = (int) (1 + Math.random() * 6);
 			sum = die1 + die2;
 		}
-		/*
-		void tryAgain() {
-			int point = sum;
-			boolean gameOver = false;
-			
-			while(gameOver == false) {
-				rollDice();
-				System.out.println("You rolled " + newGame.die1 + " + " + newGame.die2 + " = " + newGame.sum);
-				
-				if(sum == 7) {
-					System.out.println("You lose");
-					gameOver = true;
-				}
-				else if(sum == point) {
-					System.out.println("You win");
-					gameOver = true;
-				}
-				
-			}
-			
-			
-			
+		void pause() {
+			for(int i = 0; i < 1000000000; i++) { if(i % 100000000 == 0) {System.out.printf("*");}}
 		}
-		*/
 	}
 
 	public static void main(String[] args) {
-		
+		Scanner input = new Scanner(System.in);
 		boolean tryAgain = false;
 		boolean gameOver = false;
+		int rollCount = 1;
+		
+		System.out.println("********************************************************");
+		System.out.println("                WELCOME TO A GAME OF CRAPS");
+		System.out.println("********************************************************");
 		
 		while(gameOver == false) {
 		GameData newGame = new GameData();
@@ -51,31 +35,35 @@ public class Craps {
 		boolean craps = newGame.sum == 2 || newGame.sum == 3 || newGame.sum == 12;
 		boolean natural = newGame.sum == 7 || newGame.sum == 11;
 		
-		System.out.println("You rolled " + newGame.die1 + " + " + newGame.die2 + " = " + newGame.sum);
+		System.out.println("\nRoll Number " + rollCount + ":");
+		System.out.println("You rolled a " + newGame.die1 + " + " + newGame.die2 + " = " + newGame.sum);
 		
 	
 		if(craps && tryAgain == false) {
-			System.out.println("You lose");
+		System.out.println("You Lose");
 			gameOver = true;
 		}
 		else if(natural && tryAgain == false) {
-			System.out.println("You win!");
+		System.out.println("You Win!");
 			gameOver = true;
 		}
 		else if(tryAgain == true && newGame.sum == 7) {
-			System.out.println("You lose");
+		System.out.println("You Lose");
 			gameOver = true;
 		}
 		else if(tryAgain == true && newGame.sum == point) {
-			System.out.println("You win!");
+		System.out.println("You Win!");
 			gameOver = true;
 		}
 		else {
-			System.out.println("Try Again");
+			rollCount++;
+		System.out.println("Point is " + newGame.sum);
+		System.out.println("Rolling Again \n");
+			//input.nextLine();
+			newGame.pause();
+			//System.out.println();
 			tryAgain = true;
 			point = newGame.sum;
-			pause();
-			//newGame.tryAgain();
 		}
 		
 		}
